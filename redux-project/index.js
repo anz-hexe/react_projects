4d93a9a880d3a68d60361869cae1275496b5344e
AnzhelinaShevchuk <anzhelina.shevchuk@gmail.com>
2022-09-12 17:42:33 +0100

    created project with using redux

---
+++ redux-project/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore, bindActionCreators } from "redux";
+import reducer from "./reducer";
+import * as actions from "./actions";
+
+ 
+const store = createStore(reducer);
+
+const { dispatch, subscribe, getState } = store;
+
+const update = () => {
+  document.getElementById("counter").textContent = getState().value;
+};
+
+subscribe(update)
+
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
+
+document.getElementById("inc").addEventListener("click", inc);
+
+document.getElementById("dec").addEventListener("click", dec);
+
+document.getElementById("rnd").addEventListener("click", () => {
+  const value = Math.floor(Math.random() * 10);
+  rnd(value);
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<></>);
